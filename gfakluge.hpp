ff2b81f199e18d2cec0aeaa4e6ef999d68b13b85
Eric T Dawson <eric.t.dawson@gmail.com>
2015-12-02 17:09:31 +0000

    Added some basic structs to hpp

---
+++ gfakluge.hpp
@@ -0,0 +1,22 @@
+
+struct header_elem{
+
+};
+
+struct sequence{
+    // A list of links to neighbors
+    
+    // Node sequence
+}
+
+struct link{
+    // Source sequence
+    //
+    // Sink sequence
+    //
+    //
+}
+
+struct contained{
+
+}
