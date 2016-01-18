f1bd2f1c70a150aa53baeb08c3d48eefc5dbb47d
Eric T Dawson <eric.t.dawson@gmail.com>
2016-01-18 00:15:33 +0000

    Added some more to class implementation

--- gfakluge.cpp
+++ gfakluge.cpp
@@ -6,3 +6,19 @@ using namespace gfak;
 GFAKluge::GFAKluge(){
 
 }
+
+GFAKluge::~GFAKluge(){
+
+}
+
+bool GFAKluge::parse_gfa_file(string filename){
+
+}
+
+bool GFAKluge::parse_gfa_file(fstream fs){
+
+}
+
+void GFAKluge::add_link(sequence_elem seq, link_elem link){
+
+}
