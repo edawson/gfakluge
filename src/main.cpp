45bde76a1544f2892c9efb07eb4e60d945c5eee8
Eric T. Dawson <eric.t.dawson@gmail.com>
2018-03-07 18:44:51 +0000

    Update help text and refactor diff_main

--- src/main.cpp
+++ src/main.cpp
@@ -10,24 +10,29 @@ int gfakluge_package_help(char** argv){
     cerr << "GFAK: manipulate GFA files from the command line." << endl <<
         "Usage: " << argv[0] << " {command} [OPTIONS] <gfa_file> [...]" << endl <<
         "commands: " << endl <<
-        "   convert: " << endl <<
-        "   diff:   " << endl <<
-        "   extract: " << endl <<
-        "   ids:  "  << endl <<
-        "   merge:  " << endl <<
-        "   sort: " << endl <<
-        "   stats: " << endl <<
-        "   subset: " << endl <<
+        "   convert: Convert between GFA 0.1 <-> 1.0 <-> 2.0" << endl <<
+        "   diff:    Determine whether two GFA files have identical graphs" << endl <<
+        "   extract: Convert the S lines of a GFA file to FASTA format." << endl <<
+        "   ids:     Coordinate the ID spaces of multiple GFA graphs."  << endl <<
+        "   concat:  Merge GFA graphs (without ID collisions)." << endl <<
+        "   sort:    Print a GFA file in HSLP / HSEFGUO order." << endl <<
+        "   stats:   Get assembly statistics (e.g. N50) for a GFA file." << endl <<
+        "   subset:  Extract the subgraph between two IDs in a graph." << endl <<
         endl;
     return 1;
 
 }
 
 void extract_help(char** argv){
-    cerr << "gfak extract: extract a FASTA file from GFA" << endl
-        << "Usage: gfak extract [ -p ] <GFA_FILE> > file.fa " << endl
+    cerr << argv[0] <<  " extract: extract a FASTA file from GFA" << endl
+        << "Usage: " << argv[0] << "extract [ -p ] <GFA_FILE> > file.fa " << endl
             << "-p / --include-paths  include paths in output (might be contigs?? We nevr know)" << endl
         << endl;
+}
+
+void diff_help(char** argv){
+    cerr << argv[0] << " diff: determine whether two GFA files are the same." << endl
+      << "Usage: " << argv[0] << " diff [] <GFA_File_1> <GFA_File_2>" << endl;
 }
 
 /**
@@ -116,7 +121,63 @@ int extract_main(int argc, char** argv){
  * in "structure" (number of links, segments, etC)
  */
 int diff_main(int argc, char** argv){
+    if (argc < 4){
+        cerr << "diff requires two GFA files as input." << endl << endl;
+        diff_help(argv);
+        exit(0);
+    }
 
+    int c;
+    optind = 2;
+    while (true){
+        static struct option long_options[] =
+        {
+            {"help", no_argument, 0, 'h'},
+            {0,0,0,0}
+        };
+    
+        int option_index = 0;
+        c = getopt_long(argc, argv, "h", long_options, &option_index);
+        if (c == -1){
+            break;
+        }
+
+        switch (c){
+            case '?':
+            case 'h':
+                diff_help(argv);
+                exit(0);
+            default:
+                abort();
+        }
+    }
+
+    GFAKluge ff;
+    GFAKluge gg;
+    ff.parse_gfa_file(argv[optind]);
+    optind++;
+    gg.parse_gfa_file(argv[optind]);
+    optind++;
+    map<std::string, sequence_elem, custom_key> seq_1 = ff.get_name_to_seq();
+    map<std::string, sequence_elem, custom_key> seq_2 = gg.get_name_to_seq();
+    map<std::string, vector<edge_elem>> e_1 = ff.get_seq_to_edges();
+    map<std::string, vector<edge_elem>> e_2 = ff.get_seq_to_edges();
+    if (seq_1.size() != seq_2.size()){
+        cerr << "Graphs have different numbers of sequences." << endl;
+        return -1;
+    }
+    else{
+        // Check if all node IDs are the same.
+
+        // Check if the edges of every node ID are the same in both graphs.
+
+        // Check that the graphs both have identical paths.
+
+        // Check fragments and other GFA2 specific items.
+
+    }
+    
+    return 0;
 }
 
 /**
@@ -159,8 +220,8 @@ int main(int argc, char** argv){
     if (argv[1] == "convert"){
         //return convert_main(argc, argv);
     }
-    else if (argv[1] == "diff"){
-
+    else if (strcmp(argv[1], "diff") == 0){
+        return diff_main(argc, argv);
     }
     else if (strcmp(argv[1], "extract") == 0){
         return extract_main(argc, argv);
