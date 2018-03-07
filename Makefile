19fff1c9bd740273a17daa56fe13b20e9288c346
Eric T. Dawson <eric.t.dawson@gmail.com>
2018-03-07 18:16:31 +0000

    Refactor extract

--- Makefile
+++ Makefile
@@ -1,60 +1,31 @@
 CXX?=g++
 CXXFLAGS:=-O3 -std=c++11 -g
-EXE:=example.exe
-LD_LIB_FLAGS=-L./src/ -L./
-LD_INC_FLAGS=-I./src/ -I./
+
 
 BIN_DIR:=bin
+BUILD_DIR:=build
 
-all: $(BIN_DIR)/gfa_sort $(BIN_DIR)/gfa_subset $(BIN_DIR)/gfa_stats $(BIN_DIR)/gfa_diff $(BIN_DIR)/gfa_merge $(BIN_DIR)/gfa_ids $(BIN_DIR)/gfa_spec_convert $(BIN_DIR)/gfa_extract lib
+LD_LIB_FLAGS=-L./src/ -L./
+LD_INC_FLAGS=-I./src/ -I./ -I./$(BUILD_DIR) -I./src/subcommand/
 
-$(BIN_DIR)/gfa_sort: src/gfa_sort.cpp lib src/gfakluge.o .GFAK_pre-build
+gfak: $(BUILD_DIR)/main.o libgfakluge.a .GFAK_pre-build
 	+$(CXX) $(CXXFLAGS) -o $@ $< $(LD_LIB_FLAGS) $(LD_INC_FLAGS) -lgfakluge
 
-$(BIN_DIR)/gfa_stats: src/gfa_stats.cpp lib src/gfakluge.o .GFAK_pre-build
-	+$(CXX) $(CXXFLAGS) -o $@ $< $(LD_LIB_FLAGS) $(LD_INC_FLAGS) -lgfakluge
+$(BUILD_DIR)/main.o: src/main.cpp .GFAK_pre-build
+	+$(CXX) $(CXXFLAGS) -c -o $@ $< $(LD_LIB_FLAGS) $(LD_INC_FLAGS) -lgfakluge
 
-$(BIN_DIR)/gfa_verify: src/gfa_verify.cpp lib src/gfakluge.o .GFAK_pre-build
-	+$(CXX) $(CXXFLAGS) -o $@ $< $(LD_LIB_FLAGS) $(LD_INC_FLAGS) -lgfakluge
+libgfakluge.a: $(BUILD_DIR)/gfakluge.o
+	ar -rs $@ $<
 
-$(BIN_DIR)/gfa_diff: src/gfa_diff.cpp lib src/gfakluge.o .GFAK_pre-build
-	+$(CXX) $(CXXFLAGS) -o $@ $< $(LD_LIB_FLAGS) $(LD_INC_FLAGS) -lgfakluge
-
-$(BIN_DIR)/gfa_merge: src/gfa_merge.cpp lib src/gfakluge.o .GFAK_pre-build
-	+$(CXX) $(CXXFLAGS) -o $@ $< $(LD_LIB_FLAGS) $(LD_INC_FLAGS) -lgfakluge
-
-$(BIN_DIR)/gfa_ids: src/gfa_ids.cpp lib src/gfakluge.o .GFAK_pre-build
-	+$(CXX) $(CXXFLAGS) -o $@ $< $(LD_LIB_FLAGS) $(LD_INC_FLAGS) -lgfakluge
-
-$(BIN_DIR)/gfa_spec_convert: src/gfa_spec_convert.cpp lib src/gfakluge.o .GFAK_pre-build
-	+$(CXX) $(CXXFLAGS) -o $@ $< $(LD_LIB_FLAGS) $(LD_INC_FLAGS) -lgfakluge
-
-$(BIN_DIR)/gfa_extract: src/gfa_extract.cpp lib src/gfakluge.o .GFAK_pre-build
-	+$(CXX) $(CXXFLAGS) -o $@ $< $(LD_LIB_FLAGS) $(LD_INC_FLAGS) -lgfakluge
-
-$(BIN_DIR)/gfa_subset: src/gfa_subset.cpp lib src/gfakluge.o .GFAK_pre-build
-	+$(CXX) $(CXXFLAGS) -o $@ $< $(LD_LIB_FLAGS) $(LD_INC_FLAGS) -lgfakluge
-
-lib: src/gfakluge.o
-	ar -rs libgfakluge.a $<
-
-test: src/main.o lib
-	$(CXX) $(CXXFLAGS) -o $@ $< $(LD_LIB_FLAGS) $(LD_INC_FLAGS) -lgfakluge
-	./test
-	diff data/q_redundant.gfa data/q_test.gfa
-	cat data/test.gfa | sort > data/x.sort; cat data/test_test.gfa | sort > data/y.sort; diff data/x.sort data/y.sort
-
-src/main.o: src/main.cpp lib
-	$(CXX) $(CXXFLAGS) -c -o $@ $< $(LD_LIB_FLAGS) $(LD_INC_FLAGS)
-
-src/gfakluge.o: src/gfakluge.cpp src/gfakluge.hpp
+$(BUILD_DIR)/gfakluge.o: src/gfakluge.cpp src/gfakluge.hpp
 	$(CXX) $(CXXFLAGS) -c -o $@ $< $(LD_LIB_FLAGS) $(LD_INC_FLAGS)
 
 .PHONY: clean all
 
 
 .GFAK_pre-build: 
-	mkdir -p bin
+	mkdir -p $(BIN_DIR)
+	mkdir -p $(BUILD_DIR)
 	touch .GFAK_pre-build
 clean:
 	$(RM) $(EXE)
@@ -64,6 +35,7 @@ clean:
 	$(RM) x.sort
 	$(RM) y.sort
 	rm -rf $(BIN_DIR)
+	rm -rf $(BUILD_DIR)
 	$(RM) .GFAK_pre-build
 	$(RM) test_test.gfa
 	$(RM) q_test.gfa
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,115 @@
 #include <getopt.h>
+#include <cstring>
 #include "gfakluge.hpp"
 
 using namespace std;
 using namespace gfak;
 
 
-void gfakluge_package_help(argv){
+int gfakluge_package_help(char** argv){
+    cerr << "GFAK: manipulate GFA files from the command line." << endl <<
+        "Usage: " << argv[0] << " {command} [OPTIONS] <gfa_file> [...]" << endl <<
+        "commands: " << endl <<
+        "   convert: " << endl <<
+        "   diff:   " << endl <<
+        "   extract: " << endl <<
+        "   ids:  "  << endl <<
+        "   merge:  " << endl <<
+        "   sort: " << endl <<
+        "   stats: " << endl <<
+        "   subset: " << endl <<
+        endl;
+    return 1;
 
 }
 
+void extract_help(char** argv){
+    cerr << "gfak extract: extract a FASTA file from GFA" << endl
+        << "Usage: gfak extract [ -p ] <GFA_FILE> > file.fa " << endl
+            << "-p / --include-paths  include paths in output (might be contigs?? We nevr know)" << endl
+        << endl;
+}
 
-/** A wrapper around convert
- * to output a fasta file from GFA input
+/**
+ * Output a fasta file from input GFA
  */
+
 int extract_main(int argc, char** argv){
+    string gfa_file = "";
+    bool include_paths = false;
 
+    if (argc < 3){
+        cerr << "No GFA file given as input." << endl << endl; 
+        extract_help(argv);
+        exit(0);
+    }
+
+    int c;
+    optind = 2;
+    while (true){
+        static struct option long_options[] =
+        {
+            {"help", no_argument, 0, 'h'},
+            {"inlude-paths", no_argument, 0, 'p'},
+            {0,0,0,0}
+        };
+    
+        int option_index = 0;
+        c = getopt_long(argc, argv, "hp", long_options, &option_index);
+        if (c == -1){
+            break;
+        }
+
+        switch (c){
+            case 'i':
+                gfa_file = optarg;
+                break;
+
+            case '?':
+            case 'h':
+                extract_help(argv);
+                exit(0);
+            case 'p':
+                include_paths= true;
+                break;
+            default:
+                abort();
+        }
+    }
+    gfa_file = argv[optind];
+
+    GFAKluge gg;
+    gg.parse_gfa_file(gfa_file);
+
+    map<string, sequence_elem, custom_key> seqs = gg.get_name_to_seq();
+
+    for (auto name_seq : seqs){
+        cout << ">" << name_seq.second.name << endl
+            << name_seq.second.sequence << endl
+            << endl;
+    }
+    // Do the same for groups/walks/paths, as the concated
+    // sequences of the oriented seq_elems create a path (e.g. a chromosome or contig)
+    if (include_paths){
+        for (auto p : gg.get_groups()){
+            stringstream pstr;
+            if (p.second.ordered){
+                
+                pstr << ">" << p.second.id << endl;
+                for (int i = 0; i < p.second.items.size(); i++){
+                    string seqname = p.second.items[i];
+                    bool ori = p.second.orientations[i];
+                    pstr << ( ori ? seqs[seqname].sequence : string((seqs[seqname]).sequence.rbegin(), (seqs[seqname]).sequence.rend()));
+                }
+                pstr << endl;
+                cout << pstr.str() << endl;
+                pstr.str("");
+            } 
+        }
+    }
+    
+	return 0;
 }
-
 
 /**
  * Return whether two GFA files are identical
@@ -34,8 +127,63 @@ int convert_main(int argc, char** argv){
 
 }
 
+int ids_main(int argc, char** argv){
+
+}
+
+int merge_main(int argc, char** argv){
+
+}
+
+int sort_main(int argc, char** argv){
+
+}
+
+int stats_main(int argc, char** argv){
+
+}
+
+
+int subset_main(int argc, char** argv){
+
+}
+
 
 int main(int argc, char** argv){
     
+    if (argc < 2){
+       cerr << "No command provided. Please provide a second argument to GFA kluge to tell it what to do." << endl;
+       return gfakluge_package_help(argv);
+    }
+
+    if (argv[1] == "convert"){
+        //return convert_main(argc, argv);
+    }
+    else if (argv[1] == "diff"){
+
+    }
+    else if (strcmp(argv[1], "extract") == 0){
+        return extract_main(argc, argv);
+    }
+    else if (argv[1] == "ids"){
+    
+    }
+    else if (argv[1] == "merge"){
+
+    }
+    else if (argv[1] == "sort"){
+
+    }
+    else if (argv[1] == "stats"){
+
+    }
+    else if (argv[1] == "subset"){
+
+    }
+    else {
+        cerr << "No command " << '"' << argv[1] << '"' << endl;
+        cerr << "Please use one of the valid gfak commands:" << endl;
+        gfakluge_package_help(argv);
+    }
     return 0;
 }
