7e6d6ba7c473e3e995f902c6b5d250bdf34556f3
Eric T. Dawson <eric.t.dawson@gmail.com>
2018-04-16 13:26:12 -0500

    Add test harness

---
+++ src/gfak_test.cpp
@@ -0,0 +1,15 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+
+using namespace std;
+using namespace gfak;
+
+TEST_CASE(""){
+    SECTION(""){
+    }
+}
+
